c554ee99723da97aefdba8d6ece98395f6c31387
rafael_dias <rafael.a.dias@outlook.com>
2021-03-15 05:55:23 -0300

    codigo todo

---
+++ Classes/Serie.cs
@@ -0,0 +1,81 @@
+using System;
+
+namespace DIO.Series
+{
+    public class Serie : EntidadeBase
+    {
+        private Genero Genero { get; set;}
+        private string Titulo  { get; set;}
+        private string Descricao  { get; set;}
+        private int Ano  { get; set;}
+        private bool Excluido {get; set;}
+
+        public Serie(int id, Genero genero, string titulo, string descricao, int ano)
+        {
+            this.Id = id;
+            this.Genero = genero;
+            this.Titulo = titulo;
+            this.Descricao = descricao;
+            this.Ano = ano;
+            this.Excluido = false;
+        }
+
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += "Gênero: " + this.Genero + Environment.NewLine;
+            retorno += "Titulo: " + this.Titulo + Environment.NewLine;
+            retorno += "Descrição: " + this.Descricao + Environment.NewLine;
+            retorno += "Ano de Início: " + this.Ano + Environment.NewLine;
+            retorno += "Excluido: " + this.Excluido + Environment.NewLine;
+            return retorno;
+        }
+
+        public string retornaTitulo()
+        {
+            return this.Titulo;
+        }
+
+        public int retornaId()
+        {
+            return this.Id;
+        }
+
+        public bool retornaExcluido()
+        {
+            return Excluido;
+        }
+
+        public void Excluir()
+        {
+            this.Excluido = true;
+        }
+
+        public static Serie dadosSerie(int id)
+        {
+            foreach( int i in Enum.GetValues(typeof(Genero)))
+            {
+                Console.WriteLine("{0}-{1}", i, Enum.GetName(typeof(Genero), i));
+            }
+            Console.Write("Digite o gênero entre as opções acima: ");
+            int entradaGenero = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o Título da Série: ");
+            string entradaTitulo = Console.ReadLine();
+
+            Console.Write("Digite o Ano de Início da Série: ");
+            int entradaAno = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite a Descrição da Série: ");
+            string entradaDescricao = Console.ReadLine();
+
+            Serie novaSerie = new Serie(id: id,
+                                        genero: (Genero)entradaGenero,
+                                        titulo: entradaTitulo,
+                                        ano: entradaAno,
+                                        descricao: entradaDescricao);
+
+            return novaSerie;
+        }
+    }
+}
---
+++ Classes/SerieRepositorio.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using DIO.Series.Interfaces;
+
+namespace DIO.Series
+{
+    public class SerieRepositorio : IRepositorio<Serie>
+    {
+        private List<Serie> listaSerie = new List<Serie>();
+
+        public void Atualiza(int id, Serie entidade)
+        {
+            this.listaSerie[id] = entidade;
+        }
+
+        public void Exclui(int id)
+        {
+            this.listaSerie[id].Excluir();
+        }
+
+        public void Insere(Serie entidade)
+        {
+            this.listaSerie.Add(entidade);
+        }
+
+        public List<Serie> Lista()
+        {
+            return this.listaSerie;
+        }
+
+        public int ProximoId()
+        {
+            return listaSerie.Count;
+        }
+
+        public Serie RetornaPorId(int id)
+        {
+            return this.listaSerie[id];
+        }
+    }
+}
